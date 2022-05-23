9b4c9264aa65816f1be6726e59a26d74da4229d8
ibrahmounk <ibrahimagariko@gmail.com>
2022-05-23 15:33:12 +0000

    Add Document Elements with D3

---
+++ README.md
@@ -0,0 +1,1 @@
+ *300 hours FreeCodeCamp data visualization course* 
---
+++ day001.js
@@ -0,0 +1,7 @@
+/*  
+Use the select method to select the body tag in the document. Then append an h1 tag to it, and add the text Learning D3 into the h1 element.
+*/
+
+d3.select("body")
+    .append("h1")
+    .text("Learning D3");
