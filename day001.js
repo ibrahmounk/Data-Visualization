c01f394deaf0397ab64b416d75ca9b1de0003f0b
ibrahmounk <ibrahimagariko@gmail.com>
2022-05-23 16:27:16 +0000

    Work with Data in D3

--- day001.js
+++ day001.js
@@ -14,5 +14,21 @@ Select a Group of Elements with D3
 Select all of the li tags in the document, and change their text to the string list item by chaining the .text() method.
 */
 
-d3.selectAll("li")
-    .text("list item")
+// d3.selectAll("li")
+//     .text("list item")
+
+
+
+/*
+Work with Data in D3
+
+Select the body node, then select all h2 elements. Have D3 create and append an h2 tag for each item in the dataset array. The text in the h2 should say New Title. Your code should use the data() and enter() methods.
+ */
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body").selectAll("h2")
+    .data(dataset)
+    .enter()
+    .append("h2")
+    .text("New Title")
