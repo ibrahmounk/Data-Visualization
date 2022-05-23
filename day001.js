bd27535bd185e6f75e5349c8f235217bf958e81a
ibrahmounk <ibrahimagariko@gmail.com>
2022-05-23 15:56:01 +0000

    Select a Group of Elements with D3

--- day001.js
+++ day001.js
@@ -1,7 +1,18 @@
 /*  
+"Add Document Elements with D3"
 Use the select method to select the body tag in the document. Then append an h1 tag to it, and add the text Learning D3 into the h1 element.
 */
 
-d3.select("body")
-    .append("h1")
-    .text("Learning D3");
+// d3.select("body")
+//     .append("h1")
+//     .text("Learning D3");
+
+
+/*
+Select a Group of Elements with D3
+
+Select all of the li tags in the document, and change their text to the string list item by chaining the .text() method.
+*/
+
+d3.selectAll("li")
+    .text("list item")
