0e4a438ec885185d358578fc89296b0b15eec8f1
ibrahmounk <ibrahimagariko@gmail.com>
2022-05-23 18:21:44 +0000

    Work with Dynamic Data in D3

--- day001.js
+++ day001.js
@@ -25,10 +25,24 @@ Work with Data in D3
 Select the body node, then select all h2 elements. Have D3 create and append an h2 tag for each item in the dataset array. The text in the h2 should say New Title. Your code should use the data() and enter() methods.
  */
 
+// const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+// d3.select("body").selectAll("h2")
+//     .data(dataset)
+//     .enter()
+//     .append("h2")
+//     .text((d) => d + ' USD')
+
+
+// Work with Dynamic Data in D3
+/*
+Change the text() method so that each h2 element displays the corresponding value from the dataset array with a single space and the string USD. For example, the first heading should be 12 USD.
+*/
+
 const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
 d3.select("body").selectAll("h2")
     .data(dataset)
     .enter()
     .append("h2")
-    .text("New Title")
+    .text((d) => d + ' USD');
